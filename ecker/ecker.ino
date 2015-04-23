63bc0a670085412b1be9bf4add0869a68148223c
Dain Joo <givepcode@e-mirim.hs.kr>
2015-04-23 11:13:02 +0000

    get sensor info and talk to serial port

--- ecker/ecker.ino
+++ ecker/ecker.ino
@@ -1,1 +1,36 @@
+int lmPin = A0;
+int photoPin = A1;
+int irPin = A2;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+int light(){
+  int x = analogRead(photoPin);
+  return x;
+}
 
+int ir(){
+  int x = analogRead(irPin);
+  return x;
+}
+
+float tempC(){
+  float raw =                 analogRead(lmPin);
+  //return ((raw * 5.0 / 1024.0 ) - 0.5 ) * 10;
+//  
+  //float percent = raw / 1023.0;
+  //float volts = percent;
+  //return 100.0 * volts;
+  return raw;
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("T:");
+  Serial.println(tempC());
+  Serial.print("L:");
+  Serial.println(light());
+  Serial.print("IR:");
+  Serial.println(ir());
+  delay(1000);
+}
